70c15bd5f7caa865d91f2ac332581994b7a5960a
Vipin Johney <vipin.johney@gmail.com>
2020-09-02 12:08:28 +0530

    Update FitbitConfiguration.cs

--- FitBitWebAuthenticator/FitBitWebAuthenticator/Fitbit/FitbitConfiguration.cs
+++ FitBitWebAuthenticator/FitBitWebAuthenticator/Fitbit/FitbitConfiguration.cs
@@ -11,7 +11,7 @@ namespace FitBitWebAuthenticator.FirBit
         public const string Callback = "com.vipxam.webauthenticator-12345://callback";
         public const string CallbackExcaped = "com.vipxam.webauthenticator-12345%3A%2F%2Fcallback";
 
-        public const string CallbackScheme = "com.vipxam.webauthenticator-652145";
+        public const string CallbackScheme = "com.vipxam.webauthenticator-12345";
         public const string Auth2Url = "https://www.fitbit.com/oauth2/authorize?";
 
         public const string ScopesEscaped = "activity%20profile";
